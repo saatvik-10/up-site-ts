ca60bde9bd8a7fdd06176d7fda2ffd2a8b7fd9cd
saatvik-10 <saatvikmadan529@gmail.com>
2025-11-30 15:58:21 +0530

    added db monorepo, website route

--- apps/server/routes/website.ts
+++ apps/server/routes/website.ts
@@ -1,8 +1,20 @@
 import { Router } from "express";
+import { db } from "db/client"
 
 const route = Router()
 
-route.post("/website")
+route.post("/website", async (req, res) => {
+    const website = await db.website.create({
+        data: {
+            url: req.body.url
+        }
+    })
+
+    res.json({
+        id: website.id
+    })
+})
+
 route.get("/status/:websiteId")
 
 export default route
