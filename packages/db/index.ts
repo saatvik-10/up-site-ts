c8e299978b68e986cac26975cf143667cf195fe0
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-01 23:13:43 +0530

    updated prisma  config

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,10 +1,17 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+
+export const db = globalThis.prisma || new PrismaClient({
+    adapter
+});
 
 if (process.env.NODE_ENV !== 'production') {
     globalThis.prisma = db;
