87663c8508089d2ee9c10840f49f1d499ce8c319
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-07 16:00:05 +0530

    added cookie parser

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import website from "./routes/website"
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser())
 
 app.use("/api", website)
 
