856d46572d0387e91e38126c8e2daaca1bb22d5c
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-07 17:04:25 +0530

    proxy

---
+++ apps/server/proxy.ts
@@ -0,0 +1,19 @@
+import type { Response, Request, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    const tk = req.cookies.token
+
+    if (!tk) {
+        res.status(401).send("Not Authenticated!")
+        return
+    }
+
+    try {
+        const payload = jwt.verify(tk, process.env.JWT_SECRET!)
+        req.userId = payload.sub as string
+        next()
+    } catch (err) {
+        return res.status(403).json({ error: "Invalid or expired token" })
+    }
+}
