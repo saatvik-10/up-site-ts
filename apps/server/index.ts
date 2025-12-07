d6e4d8c5d947536be6910614a7f73e7730fa3d3f
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-07 16:48:38 +0530

    added user route path

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import website from "./routes/website"
+import user from "./routes/user"
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use("/api", website)
+app.use("/api", user)
 
 app.listen(8080)
