bf2662267c42ac132e6e75be76b91dc4afbda69b
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-11 01:08:01 +0530

    updated file execution

--- packages/redis-streams/index.ts
+++ packages/redis-streams/index.ts
@@ -2,11 +2,14 @@ import { createClient } from 'redis';
 import { chunkArray } from './utils/lib';
 
 interface WebsiteProps {
-  id: string;
-  url: string;
+    id: string;
+    url: string;
 }
 
-const client = createClient();
+const client = await createClient()
+    .on('error', (err) => console.log('Redis Client Error', err)).connect()
+
+console.log('Redis client connected');
 
 // export async function xAdd({ id, url }: WebsiteProps) {
 //   await client.xAdd('upsite:websites', '*', {
@@ -16,29 +19,33 @@ const client = createClient();
 // }
 
 export async function xAddBulk(websites: WebsiteProps[], batchSize = 100) {
-  try {
-    await client.connect();
-    console.log('Redis bulk streaming connected');
+    try {
+        await client.connect();
+        console.log('Redis bulk streaming connected');
 
-    const validWebsites = websites.filter(
-      (website) => website.id && website.url
-    );
-    if (validWebsites.length == 0) return;
+        const validWebsites = websites.filter(
+            (website) => website.id && website.url
+        );
+        if (validWebsites.length == 0) return;
 
-    const chunks = chunkArray(validWebsites, batchSize);
+        const chunks = chunkArray(validWebsites, batchSize);
 
-    for (const chunk of chunks) {
-      const pipeline = client.multi();
+        for (const chunk of chunks) {
+            try {
+                const pipeline = client.multi();
 
-      for (const website of chunk) {
-        pipeline.xAdd('upsite:websites', '*', {
-          id: website.id,
-          url: website.url,
-        });
-      }
-      await pipeline.exec();
+                for (const website of chunk) {
+                    pipeline.xAdd('upsite:websites', '*', {
+                        id: website.id,
+                        url: website.url,
+                    });
+                }
+                await pipeline.exec();
+            } catch (err) {
+                console.error('Batch failed:', err);
+            }
+        }
+    } catch (err) {
+        console.log('Err connecting redis bulk streaming');
     }
-  } catch (err) {
-    console.log('Err connecting redis bulk streaming');
-  }
 }
