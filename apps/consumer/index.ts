062b831aaaec7ddb0bc147f4d0106cedec69a770
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-11 23:24:43 +0530

    added worker tick updater

--- apps/consumer/index.ts
+++ apps/consumer/index.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import { createClient } from 'redis';
 import { xAck, xReadGroup } from 'redis-streams/client';
-
-const client = createClient();
+import { db } from "db/client"
 
 const REGION_ID = process.env.REGION_ID!;
 const WORKER_ID = process.env.WORKER_ID!;
@@ -16,10 +14,49 @@ if (!WORKER_ID) {
 }
 
 async function worker() {
-    // while (true) {
-        //reading, processing and storing via queue in bulk, acknowledgement to the queue
-        const res = xReadGroup(REGION_ID, WORKER_ID)
+    //reading, processing and storing via queue in bulk, acknowledgement to the queue
+    const res = await xReadGroup(REGION_ID, WORKER_ID)
 
-        xAck(REGION_ID, '0');
-    // }
+    let promises = res
+        .filter(({ message }) => message.id && message.url)
+        .map(({ message }) => fetchUpTicks(message.id!, message.url!))
+
+    await Promise.all(promises)
+
+    xAck(REGION_ID, '0');
+}
+
+worker()
+
+async function fetchUpTicks(websiteId: string, url: string) {
+    return new Promise<void>((resolve, reject) => {
+
+        const startTime = Date.now()
+
+        axios.get(url).then(async () => {
+            const endTime = Date.now()
+
+            await db.website_tick.create({
+                data: {
+                    response_time_ms: endTime - startTime,
+                    status: "Up",
+                    region_id: REGION_ID,
+                    website_id: websiteId
+                }
+            })
+            resolve()
+        }).catch(async () => {
+            const endTime = Date.now()
+
+            await db.website_tick.create({
+                data: {
+                    response_time_ms: endTime - startTime,
+                    status: "Down",
+                    region_id: REGION_ID,
+                    website_id: websiteId
+                }
+            })
+            resolve()
+        })
+    })
 }
