d7902c4495c5caf60d3006581e6aff0f35ca9535
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-14 15:06:33 +0530

    added cors

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import website from "./routes/website"
 import user from "./routes/user"
-import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 
 app.use(cookieParser())
 
