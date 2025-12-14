17fcb1060bbc1ec8af2529ceaf5a05af1f107a01
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-14 18:07:46 +0530

    btn change with auth state

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -1,20 +1,23 @@
-'use client'
+'use client';
 
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
+import { useAuth } from '@/lib/useAuth';
 
 const Navbar = () => {
-  const router = useRouter();
+  const route = useRouter();
 
   const handleAuthType = (authType: 'sign-in' | 'sign-up') => {
-    router.push(`/${authType}`);
+    route.push(`/${authType}`);
   };
+
+  const { isAuthenticated, signOut } = useAuth();
 
   return (
     <header className='animate-slide-down fixed top-0 left-0 right-0 z-50'>
       <nav className='mx-auto max-w-7xl px-6 py-4'>
-        <div className='flex items-center justify-between backdrop-blur-md bg-surface/60 border border-border rounded-full px-6 py-3'>
+        <div className='flex items-center justify-between backdrop-blur-md bg-surface/60 border border-border rounded-xl px-6 py-3'>
           <Link
             href='/'
             className='flex items-center gap-3 group cursor-pointer'
@@ -26,21 +29,43 @@ const Navbar = () => {
             <span className='text-xl font-bold tracking-tight'>UpSite</span>
           </Link>
 
-          <div className='flex items-center gap-3'>
-            <Button
-              variant='ghost'
-              className='text-sm font-medium hidden sm:flex hover:bg-primary/60 cursor-pointer'
-              onClick={() => handleAuthType('sign-in')}
-            >
-              Sign In
-            </Button>
-            <Button
-              className='text-sm font-semibold bg-primary text-primary-foreground hover:bg-primary/60 rounded-full px-5 cursor-pointer'
-              onClick={() => handleAuthType('sign-up')}
-            >
-              Get Started
-            </Button>
-          </div>
+          {!isAuthenticated ? (
+            <>
+              <div className='flex items-center gap-3'>
+                <Button
+                  variant='ghost'
+                  className='text-sm font-semibold hidden sm:flex bg-primary text-primary-foreground hover:bg-primary/60 cursor-pointer rounded-lg'
+                  onClick={() => handleAuthType('sign-in')}
+                >
+                  Sign In
+                </Button>
+                <Button
+                  className='text-sm font-semibold bg-primary text-primary-foreground hover:bg-primary/60 rounded-lg px-5 cursor-pointer'
+                  onClick={() => handleAuthType('sign-up')}
+                >
+                  Get Started
+                </Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className='flex items-center gap-3'>
+                <Button
+                  variant='ghost'
+                  className='text-sm font-semibold hidden sm:flex text-primary-foreground bg-primary hover:bg-primary/60 cursor-pointer rounded-lg'
+                  onClick={() => route.push('/dashboard')}
+                >
+                  Dashboard
+                </Button>
+                <Button
+                  className='text-sm font-semibold bg-destructive text-primary hover:bg-destructive/60 rounded-lg px-5 cursor-pointer'
+                  onClick={signOut}
+                >
+                  Sign out
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </nav>
     </header>
