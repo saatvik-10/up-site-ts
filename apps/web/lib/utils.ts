33fa6e2109b153534a87054467b0c0a8801acc72
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-22 14:59:53 +0530

    utils

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getStatusColor = (status?: string) => {
+  switch (status) {
+    case 'Up':
+      return 'bg-accent';
+    case 'Down':
+      return 'bg-destructive';
+    default:
+      return 'bg-muted-foreground';
+  }
+};
+
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+};
