2f6f77155d65aaeb03ed92202a9d06293db660bf
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-22 21:37:09 +0530

    added loader

--- apps/web/lib/AuthProvider.tsx
+++ apps/web/lib/AuthProvider.tsx
@@ -2,12 +2,17 @@
 'use client';
 
 import { useAuth } from './useAuthStore';
+import { Spinner } from '@/components/ui/spinner';
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const { isLoading } = useAuth();
 
   if (isLoading) {
-    return <div>Loading...</div>; // spinner later
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Spinner className='h-20 w-20' />
+      </div>
+    );
   }
 
   return <>{children}</>;
