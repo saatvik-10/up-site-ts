e4a2de18cafed9048f1dced9317edfedf6d3fbbc
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-24 15:39:14 +0530

    added alert redis stream

--- packages/redis-streams/alert.ts
+++ packages/redis-streams/alert.ts
@@ -1,6 +1,16 @@
 import { createClient } from 'redis';
 import type { Alert } from './types';
 
+interface AlertMsg {
+  id: string;
+  message: Record<string, string>;
+}
+
+interface RedisStreamResponse {
+  name: string;
+  messages: AlertMsg[];
+}
+
 const ALERTSTREAM = 'upsite:alerts';
 
 const client = await createClient({
@@ -11,7 +21,7 @@ const client = await createClient({
 
 console.log('Redis client connected');
 
-export async function xAdd({
+export async function xAddAlert({
   alert_id,
   website_id,
   user_id,
@@ -25,4 +35,52 @@ export async function xAdd({
     alert_type: alert_type,
     triggered_at: triggered_at.toString(),
   });
+}
+
+export async function xCreateAlertGroup(consumerGrp: string) {
+  try {
+    await client.xGroupCreate(ALERTSTREAM, consumerGrp, '0', {
+      MKSTREAM: true,
+    });
+  } catch (err: any) {
+    console.log(err);
+  }
+}
+
+export async function xReadAlertGroup(
+  consumerGrp: string,
+  workerId: string
+): Promise<AlertMsg[]> {
+  const res = (await client.xReadGroup(
+    consumerGrp,
+    workerId,
+    {
+      key: ALERTSTREAM,
+      id: '>',
+    },
+    {
+      COUNT: 5,
+    }
+  )) as RedisStreamResponse[] | null;
+
+  if (!res || res.length == 0) {
+    return [];
+  }
+
+  const alerts: AlertMsg[] = res[0]!.messages.map((alert: any) => ({
+    id: alert.id,
+    message: {
+      alert_id: alert.message.alert_id,
+      website_id: alert.message.website_id,
+      user_id: alert.message.user_id,
+      alert_type: alert.message.alert_type,
+      triggered_at: alert.message.triggered_at,
+    },
+  }));
+
+  return alerts;
+}
+
+export async function xAckAlert(consumerGrp: string, eventId: string) {
+  await client.xAck(ALERTSTREAM, consumerGrp, eventId);
 }
