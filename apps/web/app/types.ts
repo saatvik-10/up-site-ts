053b12f747d404ea76a3e0c87d656b36e3937376
saatvik-10 <saatvikmadan529@gmail.com>
2025-12-30 01:26:45 +0530

    added alert type

--- apps/web/app/types.ts
+++ apps/web/app/types.ts
@@ -3,6 +3,7 @@ export interface Website {
   url: string;
   time_added: string;
   ticks?: WebsiteTick[];
+  alerts?: Alert[];
 }
 
 export interface WebsiteTick {
@@ -10,4 +11,10 @@ export interface WebsiteTick {
   response_time_ms: number;
   status: 'Up' | 'Down' | 'Unknown';
   created_at: string;
+}
+
+export interface Alert {
+  id: string;
+  status: 'Up' | 'Down' | 'Unknown';
+  time_added: string;
 }
